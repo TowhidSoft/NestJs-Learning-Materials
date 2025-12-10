c552dcfdc8c5bbc25df0cbd8c167fe9da7bfd62c
TowhidSoft <devtowhid486@gmail.com>
2025-12-10 22:24:46 +0600

    controller, service, module explained

--- my-nest-app/src/app.module.ts
+++ my-nest-app/src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
+import { EmployeeModule } from './employee/employee.module';
+
 
 @Module({
-  imports: [],
+  imports: [EmployeeModule],
   controllers: [AppController, UserController],
   providers: [AppService, UserService],
 })
-export class AppModule {}
+export class AppModule { }
